981507087d4ff821d1d289cfe996942021c36227
Kennedy777 <46945500+Kennedy777@users.noreply.github.com>
2019-04-04 05:21:56 -0700

    Add initial html, css, and js files, as well as a README

---
+++ scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+
+    var whichEnd = $('input[name=endType]:checked').val();
+    var where = $('input[name=location]:checked').val();
+    var langType = $('input[progType]:checked').val();
+    var company = $('input[bizType]:checked').val();
+
+    if(whichEnd === "front") {
+      $(".result").text("Take the UX/UI and design track. Hope you're in Portland, Oregon!");
+    } else if (whichEnd === "back" || money === "mix") {
+      if(company === "corp") {
+        $(".result").text("C# or Java would be great for you!");
+      } else { // relax === relax || relax === mix
+            $(".result").text("Ruby on Rails would be a great starting point!"););
+        }
+      }
+  //  } else { // money === not an issue
+    //  $(".result").text("Antartica, Dubai or Japan");
+//    }
+$('#quest1 #submit').click() {
+   $('.quizForm').hide();
+   $('#quest2').fadeIn(200);
+   return false;
+  }
+
+$('#quest2 #submit').click() {
+  $('.quizForm').hide();
+  $('#quest3').fadeIn(300);
+  return false;
+  }
+
+$('#quest3 #submit').click() {
+  $('.quizForm').hide();
+   $('quest4').fadeIn(300);
+   return false;
+ }
+
+$('#quest4 #submit').click() {
+  $('.quizForm').hide();
+  $('#quest5').fadeIn(300);
+  return false;
+ }
+
+  $('#quest5 #submit').click() {
+    $('.quizForm').hide();
+    $('#result').fadein(200)
+
+  }
+    event.preventDefault();
+  });
+});
